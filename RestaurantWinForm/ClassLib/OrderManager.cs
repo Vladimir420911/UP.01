8394aa9db47a0571b22251945aa05a4bd410a25e
Vladimir420911 <dhbtgvhb@gmail.com>
2025-10-20 15:27:28 +0300

    Добавлен интерфейс IOrderRepository, а также класс OrderManager реализующий данный интерфейс

---
+++ RestaurantWinForm/ClassLib/OrderManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class OrderManager : IOrderRepository
+    {
+
+        private IOrderRepository _orderRepository;
+        public OrderManager(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public string AddOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order CreateOrder(int seatId, List<OrderItem> items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
