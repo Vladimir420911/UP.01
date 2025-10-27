c46da2b80afbc1c1a5cef489fbc03f42eca20d52
Vladimir420911 <dhbtgvhb@gmail.com>
2025-10-27 16:00:14 +0300

    Добавил интерфейс IStaffRepository и класс AuthManager

---
+++ RestaurantWinForm/ClassLib/AuthManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class AuthManager
+    {
+        private readonly IStaffRepository _staffRepository;
+
+        public AuthManager(IStaffRepository staffRepository)
+        {
+            _staffRepository = staffRepository;
+        }
+
+        public Staff CurrentUser { get; private set; }
+
+        public bool Login(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                CurrentUser = null;
+                return false;
+            }
+
+            var staff = _staffRepository.GetByUsername(username);
+            if (staff == null || staff.Password != password)
+            {
+                CurrentUser = null;
+                return false;
+            }
+
+            CurrentUser = staff;
+            return true;
+        }
+
+        public void Logout()
+        {
+            CurrentUser = null;
+        }
+    }
+}
