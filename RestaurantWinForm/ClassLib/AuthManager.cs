4bee84fc48fa11d7cf411d94596416dea4b9c4ff
Vladimir420911 <dhbtgvhb@gmail.com>
2025-11-06 17:29:00 +0300

    Добавил интерфейс IStaffRepository и реализующий его класс AuthManager

---
+++ RestaurantWinForm/ClassLib/AuthManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class AuthManager : IStaffRepository
+    {
+        public LoginResult Login(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
