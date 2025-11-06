acff744ee9c209ec6f3b02589b6126b8bd28bbdd
Vladimir420911 <dhbtgvhb@gmail.com>
2025-11-06 17:20:37 +0300

    Добавил класс Staff, описывающий работника ресторана и перечисление, описывающее роль пользователя

---
+++ RestaurantWinForm/ClassLib/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class Staff
+    {
+        private int StaffId;
+        public string Login { get; set; }
+        private string Password;
+        public string UserName { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
