81f92c6ac51e2e27c7488e2146e72080f1c0bc74
Vladimir420911 <dhbtgvhb@gmail.com>
2025-11-07 10:03:44 +0300

    Добавил заголовки для формы повара и официанта

--- RestaurantWinForm/RestaurantWinForm/CookForm.Designer.cs
+++ RestaurantWinForm/RestaurantWinForm/CookForm.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "CookForm";
-            this.Text = "Form1";
+            this.Text = "Интерфейс повара";
             this.ResumeLayout(false);
 
         }
