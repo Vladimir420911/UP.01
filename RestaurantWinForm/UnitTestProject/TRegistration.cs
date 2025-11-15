e160251cf4f235da5a7e71cc51f4c68a2f1071f2
Vladimir420911 <dhbtgvhb@gmail.com>
2025-11-15 09:06:12 +0300

    Реализовано тестирование успешной регистрации пользователя

--- RestaurantWinForm/UnitTestProject/TRegistration.cs
+++ RestaurantWinForm/UnitTestProject/TRegistration.cs
@@ -1,15 +1,53 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ClassLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using Moq;
 
 namespace UnitTestProject
 {
     [TestClass]
     public class TRegistration
     {
+        private readonly List<Staff> _existingStaff = new List<Staff>();
+        [TestInitialize]
+        public void TestInit()
+        {
+            var staff1 = new Staff(1, "1234");
+            staff1.UserName = "user1";
+            staff1.Login = "login1";
+            staff1.Role = UserRole.Waiter;
+
+            _existingStaff.Add(staff1);
+
+            var staff2 = new Staff(2, "12345");
+            staff2.UserName = "user2";
+            staff2.Login = "login2";
+            staff2.Role = UserRole.Waiter;
+
+            _existingStaff.Add(staff2);
+
+            var staff3 = new Staff(3, "123456");
+            staff3.UserName = "user3";
+            staff3.Login = "login3";
+            staff3.Role = UserRole.Waiter;
+
+            _existingStaff.Add(staff3);
+        }
         [TestMethod]
         public void TRegisterReturnsSuccess()
         {
+            var mockRepo = new Mock<IStaffRepository>();
 
+            mockRepo.Setup(repo => repo.GetUserByLogin("newSotrudnikLogin"))
+                    .Returns((Staff)null);
+
+            mockRepo.Setup(repo => repo.Register("newSotrudnik", "newSotrudnikLogin", "123", UserRole.Waiter))
+                    .Returns(RegistrationResult.Success);
+
+            var result = mockRepo.Object.Register("newSotrudnik", "newSotrudnikLogin", "123", UserRole.Waiter);
+
+            Assert.AreEqual(RegistrationResult.Success, result);
         }
     }
 }
